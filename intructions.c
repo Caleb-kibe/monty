1ba517ea46aeb55ab34f7dd12a0e798536eec12a
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-01-20 16:26:05 +0300

    fetches an opcode that corresponds to an instruction

---
+++ intructions.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * get_opcode - fetches the opcode that corresponds to the instruction
+ * @opcode: function that performs an instruction
+ * @line_number: line containing the opcode
+ * @stack: pointer pointing to the top of the stack
+ * @argument: the instruction being passed
+ */
+
+void get_opcode(char *opcode, unsigned int line_number, stack_t **stack, int argument)
+{
+	int i = 0;
+
+	instruction_t intructions[] = {
+		{"push", push_opcode},
+		{"pall", pall_opcode},
+		{NULL, NULL}
+	};
+
+	if (strcmp(opcode, "push") == 0)
+	{
+		push(stack, line_number, argument);
+		return;
+	}
+
+	while (i != '\0')
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
