86b3443446e634fcb5fdabc7cea80a77a7b4df2c
Pury-01 <puryokumu@gmail.com>
2024-01-20 14:34:24 +0300

    function that pushes elements to the stack and dispalys them to stdout

--- functions1.c
+++ functions1.c
@@ -9,32 +9,50 @@
 void push_opcode(stack_t **stack, unsigned int line_number)
 {
 
-        stack_t *newnode, *temp;
-        stack_t *top = *stack;
-        unsigned int data;
+	stack_t *newnode, *temp;
+	stack_t *top = *stack;
+	unsigned int data;
 
 	newnode = (stack_t *)malloc(sizeof(stack_t));
-        if (newnode == NULL)
+	if (newnode == NULL)
 	{
 		fprintf(stderr,"Error: malloc failed\n");
-                exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
-        newnode->prev = 0;
-        newnode->next = 0;
-        newnode->n = data;
+	newnode->prev = 0;
+	newnode->next = 0;
+	newnode->n = data;
 
-        if (top == 0)
-        {
-                top = newnode;
-                temp = newnode;
-        }
-        else
-        {
-                temp->next = newnode;
-                newnode->prev = temp;
-                temp = newnode;
-        }
+	if (top == 0)
+	{
+		top = newnode;
+		temp = newnode;
+	}
+	else
+	{
+		temp->next = newnode;
+		newnode->prev = temp;
+		temp = newnode;
+	}
 	*stack = top;
-        
+
+}
+/**
+ *pall - displays elements of the stack
+ *@stack: pointer to the top of the stack
+ *@line_number: the line containing the instructions
+ *
+ *Return: nothing 
+ */
+void pall_opcode(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	temp = top;
+
+	while (temp != 0)
+	{
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
 }
