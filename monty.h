bbb1e943756f7ffd2b84765ba68f5ec243a275b2
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-01-21 21:29:35 +0300

    header file

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void get_opcode(char *opcode, unsigned int line_number, stack_t **stack, int argument);
+void get_opcode(char *opcode, unsigned int line_number, stack_t **stack, char *argument);
 void push_opcode(stack_t **stack, unsigned int line_number);
 void pall_opcode(stack_t **stack, unsigned int line_number);
 
