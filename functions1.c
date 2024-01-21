008e76cb47ff74331f1ce8f32010f012b2e144dd
Pury-01 <puryokumu@gmail.com>
2024-01-21 22:45:18 +0300

    monty functions

--- functions1.c
+++ functions1.c
@@ -5,21 +5,30 @@
  *@stack: pointer to the top the stack
  *@line_number: where the instruction appears
  */
-
 void push_opcode(stack_t **stack, unsigned int line_number)
 {
-
-	stack_t *newnode, *temp;
+	stack_t *newnode, *temp = NULL;
 	stack_t *top = *stack;
-	unsigned int data;
+	unsigned int data = 0;
+	char *argument = strtok(NULL, "\n");
+	int n = atoi(argument);
 
+	if (argument == NULL)
+	{
+		fprintf(stderr, "L%u: usage:push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (n == 0 && *argument != '0')
+	{
+		fprintf(stderr, "L%u: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
 	newnode = (stack_t *)malloc(sizeof(stack_t));
 	if (newnode == NULL)
 	{
-		fprintf(stderr,"Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	newnode->prev = 0;
 	newnode->next = 0;
 	newnode->n = data;
@@ -35,24 +44,30 @@ void push_opcode(stack_t **stack, unsigned int line_number)
 		newnode->prev = temp;
 		temp = newnode;
 	}
-	*stack = top;
-
+	top = newnode;
 }
+
 /**
- *pall - displays elements of the stack
+ *pall_opcode - displays elements of the stack
  *@stack: pointer to the top of the stack
  *@line_number: the line containing the instructions
  *
- *Return: nothing 
+ *Return: nothing
  */
 void pall_opcode(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-	temp = top;
+	char *argument = strtok(NULL, "\n");
+
+	if (argument == NULL)
+	{
+		fprintf(stderr, "L%u: unknown instruction opcode", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	while (temp != 0)
 	{
-		printf("%d\n", temp->data);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 	}
 }
